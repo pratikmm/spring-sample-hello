b99ce1751d026799486ac92a86d151e1648e19dc
pratikmm7 <pratik.mungekar@deliveryhero.com>
2022-01-16 14:13:47 +0100

    first commit

---
+++ src/main/java/hello/Apis/TestController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public ResponseEntity testhello(){
+
+
+        String json = "{'status': 'success'}";
+        return new ResponseEntity<>(json, HttpStatus.OK);
+
+    }
+
+}
